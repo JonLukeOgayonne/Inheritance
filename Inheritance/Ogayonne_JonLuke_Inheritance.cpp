b576cae78f0a3d6691e8dfb5085161dc6a96f355
Jon-Luke Ogayonne <jon-luke.ogayonne@sinclair.edu>
2021-04-26 16:55:00 -0400

    Added a Vehicle header file to my program. I also made some cin and cout statements to the main function.

--- Inheritance/Ogayonne_JonLuke_Inheritance.cpp
+++ Inheritance/Ogayonne_JonLuke_Inheritance.cpp
@@ -4,11 +4,27 @@
 	April 26, 2021
 */
 #include<iostream>
+#include<string>
+#include "Vehicle.h"
 using namespace std;
 
 int main() {
+	// String and integer to hold the automobile data.
+	string modelName;
+	int year;
 	cout << " Vehicle Program\n ";
-
+	cout << " Vehicle:";
+	Vehicle vehicle();
+	//
+	cout << " Enter the manufacturer: ";
+	cin >> modelName;
+	cout << " Enter the year built: ";
+	cin >> year;
+	//
+	cout << " Vehicle information:\n Manufacturer: ";
+	cout << ;
+	cout << " Year Built: ";
+	cout << ;
 
 	return 0;
 }
