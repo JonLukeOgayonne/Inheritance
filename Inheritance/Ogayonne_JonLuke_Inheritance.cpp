0de595a98586c13d4f3f06e4f423bd638c980453
Jon-Luke Ogayonne <jon-luke.ogayonne@sinclair.edu>
2021-04-26 17:24:29 -0400

    I have now completed the first third of the expected output of this program by adding a getline and a display method to the main cpp file.

--- Inheritance/Ogayonne_JonLuke_Inheritance.cpp
+++ Inheritance/Ogayonne_JonLuke_Inheritance.cpp
@@ -12,19 +12,20 @@ int main() {
 	// String and integer to hold the automobile data.
 	string modelName;
 	int year;
-	cout << " Vehicle Program\n ";
+	cout << " Vehicle Program\n\n";
 	cout << " Vehicle:";
 	Vehicle vehicle();
 	//
-	cout << " Enter the manufacturer: ";
-	cin >> modelName;
+	cout << "\n Enter the manufacturer: ";
+	getline(cin, modelName);
 	cout << " Enter the year built: ";
 	cin >> year;
 	//
-	cout << " Vehicle information:\n Manufacturer: ";
-	cout << ;
-	cout << " Year Built: ";
-	cout << ;
+	Vehicle myVehicle(modelName, year);
+	//
+	cout << " Vehicle information:" << endl;
+	cout << " Manufacturer: " << myVehicle.getManufacturerName() << endl;
+	cout << " Year Built: " << myVehicle.getYearBuilt() << endl;
 
 	return 0;
 }
