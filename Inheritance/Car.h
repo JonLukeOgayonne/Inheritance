8a79a70de1d89c531aa7ab857162ca8fe23a518f
Jon-Luke Ogayonne <jon-luke.ogayonne@sinclair.edu>
2021-04-26 21:07:58 -0400

    I update the program to give and retrieve data for the Car class header which was also created.

---
+++ Inheritance/Car.h
@@ -0,0 +1,24 @@
+#ifndef CAR_H
+#define CAR_H
+#include<string>
+#include "Vehicle.h"	// Vehicle header is inherited.
+using namespace std;
+// Class Car declaration.
+class Car : public Vehicle {
+private:		// Private integer member for Car class.
+	int numOfDoors;
+public:
+	// Default constructor.
+	Car() : Vehicle() {
+		numOfDoors = 0;
+	}
+	// Constructor for Car class.
+	Car(string vehicleManufacturer, int vehicleyear, int doors) : Vehicle(vehicleManufacturer, vehicleyear) {
+		numOfDoors = doors;
+	}
+	// Accessor for door integer.
+	int getDoors() {
+		return numOfDoors;
+	}
+};
+#endif // End Car declaration.
--- Inheritance/Ogayonne_JonLuke_Inheritance.cpp
+++ Inheritance/Ogayonne_JonLuke_Inheritance.cpp
@@ -6,12 +6,13 @@
 #include<iostream>
 #include<string>
 #include "Vehicle.h"
+#include "Car.h"
 using namespace std;
 
 int main() {
 	// String and integer to hold the automobile data.
 	string modelName;
-	int year;
+	int year, doors;
 	cout << " Vehicle Program\n\n";
 	cout << " Vehicle:";
 	Vehicle vehicle();
@@ -20,12 +21,30 @@ int main() {
 	getline(cin, modelName);
 	cout << " Enter the year built: ";
 	cin >> year;
+	cin.ignore();
 	//
 	Vehicle myVehicle(modelName, year);
 	//
 	cout << " Vehicle information:" << endl;
 	cout << " Manufacturer: " << myVehicle.getManufacturerName() << endl;
 	cout << " Year Built: " << myVehicle.getYearBuilt() << endl;
-
+	//
+	cout << "\n\n Car:";
+	Car car();
+	//
+	cout << "\n Enter the manufacturer: ";
+	getline(cin, modelName);
+	cout << " Enter the year built: ";
+	cin >> year;
+	cout << " Enter the number of doors: ";
+	cin >> doors;
+	//
+	Car myCar(modelName, year, doors);
+	//
+	cout << " Vehicle information:" << endl;
+	cout << " Manufacturer: " << myCar.getManufacturerName() << endl;
+	cout << " Year Built: " << myCar.getYearBuilt() << endl;
+	cout << " Doors: " << myCar.getDoors() << endl;
+	// Terminate program.
 	return 0;
 }
